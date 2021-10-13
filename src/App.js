70654ae95118a66aa1e90cc94624e141174c7169
marinaBergas <65818690+marinaBergas@users.noreply.github.com>
2021-10-13 22:32:10 +0200

    hash-route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
 import "./App.scss";
 import FormFieldset from "./components/form-fieldset/FormFieldset";
@@ -7,11 +7,12 @@ import RequestsPage from "./components/requests page/requestsPage";
 function App() {
   return (
     <div className="App">
-      <RequestsPage />
-      <Router>
-        <Route path="/form" exact component={FormFieldset} />
-        <Route path="/" exact component={RequestsPage} />
-      </Router>
+      <HashRouter>
+        <Switch>
+          <Route path="/form" exact component={FormFieldset} />
+          <Route path="/" exact component={RequestsPage} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
